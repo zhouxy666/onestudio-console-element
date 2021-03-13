8b7e0c30ec5d35ae65cc05fa9a86f1e2a1f8aa05
zhouxy <zhouxy_123@126.com>
2021-03-13 16:04:59 +0800

    升级vue3.0

---
+++ src/new-views/members/courseware/CoursewarePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="courseware-wrapper"></div>
+</template>
+
+<script>
+import {
+  reactive,
+  ref,
+  computed,
+  watch,
+  onMounted
+} from "@vue/composition-api";
+export default {
+  setup() {}
+};
+</script>
+'
+<style lang="sass" scoped></style>
